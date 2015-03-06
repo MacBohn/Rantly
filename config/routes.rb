654fc892fba644d4539214749335cbbf80c8b5f5
McCartney Bohn <mccartneybohn@yahoo.com>
2015-03-05 17:45:01 -0700

    added rants controller model and route and migration

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   get '/', to: redirect('/')
 
   resources :users
+  resources :rants
 
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,10 +1,20 @@
-User.create!(
+User.delete_all
+Rant.delete_all
+
+admin = User.create!(
 first_name: "Admin",
 last_name: "Admin",
 email: "admin@example.com",
 password: "password"
 )
 
+5.times do |i|
+Rant.create!(
+  title: Faker::Lorem.words(4),
+  body: Faker::Lorem.paragraph,
+  user_id: admin.id
+)
+end
 
 30.times do |i|
   User.create!(
@@ -13,5 +23,6 @@ password: "password"
   email: Faker::Internet.email,
   password: Faker::Lorem.word
   )
+
 
 end
