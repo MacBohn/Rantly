e9429103c332aad396db868c5388c905b4288c56
McCartney Bohn <mccartneybohn@yahoo.com>
2015-03-06 17:06:00 -0700

    Added update in the user controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -15,12 +15,17 @@ class UsersController < ApplicationController
     @user.save
     render json: @user
   end
-  
+
+  def update
+    @user = User.find(params[:id])
+    @user.update(user_params)
+    render json: @user
+  end
 
   private
 
   def user_params
-      params.require(:user).permit(:fisrt_name, :last_name, :email, :password)
+      params.require(:user).permit(:first_name, :last_name, :email, :password)
   end
 
 end
